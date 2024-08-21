6793b525da8ec063fa5444c76b05d72f21a923ba
Joshua Simionato <joshua.simionato@gmail.com>
2024-08-21 12:16:50 -0300

    Componente Home - CSS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from "./pages/home/Home"
+
+function App() {
+  // Código TypeScript
+  return (
+    // Código TSX => HtML + CSS
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
