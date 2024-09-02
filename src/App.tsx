b2b1d67c59bf291edbff311734033e383b29c6f9
Joshua Simionato <joshua.simionato@gmail.com>
2024-09-02 16:30:48 -0300

    cadastrar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import FormularioPostagem from "./components/postagens/formularioPostagem/FormularioPostagem";
+import ListaPostagens from "./components/postagens/listaPostagens/ListaPostagens";
+import DeletarTema from "./components/temas/deletartema/DeletarTema";
+import FormularioTema from "./components/temas/formularioTema/FormularioTema";
+import ListaTemas from "./components/temas/listaTemas/ListaTemas";
+import { AuthProvider } from "./contexts/AuthContext";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/login/Login';
-import Cadastro from './pages/cadastro/Cadastro';
-import Home from './pages/home/Home';
-import { AuthProvider } from './contexts/AuthContext';
-import ListaTemas from './components/temas/listaTemas/ListaTemas';
-import FormularioTema from './components/temas/formularioTema/FormularioTema';
-import DeletarTema from './components/temas/deletartema/DeletarTema';
 
 
 function App() {
   return (
     <>
-    <AuthProvider>
+      <AuthProvider>
         <BrowserRouter>
           <Navbar />
-          <div className='min-h-[80vh]'>
+          <div className="min-h-[80vh]">
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
@@ -29,11 +29,15 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-        </AuthProvider>
+      </AuthProvider>
     </>
   );
 }
--- src/components/temas/listaTemas/ListaTemas.tsx
+++ src/components/temas/listaTemas/ListaTemas.tsx
@@ -5,11 +5,12 @@ import { AuthContext } from '../../../contexts/AuthContext';
 import Tema from '../../../models/Tema';
 import { buscar } from '../../../services/Service';
 import CardTemas from '../cardTemas/CardTemas';
-
+//
 function ListaTemas() {
   const [temas, setTemas] = useState<Tema[]>([]);
 
-  const navigate = useNavigate();
+  let navigate = useNavigate();
+
   const { usuario, handleLogout } = useContext(AuthContext);
   const token = usuario.token;
 
@@ -19,7 +20,8 @@ function ListaTemas() {
         headers: { Authorization: token },
       });
     } catch (error: any) {
-      if (error.toString().includes('401')) {
+      if (error.toString().includes('403')) {
+        alert('O token expirou, favor logar novamente')
         handleLogout()
       }
     }
@@ -28,7 +30,7 @@ function ListaTemas() {
   useEffect(() => {
     if (token === '') {
       alert('Você precisa estar logado');
-      navigate('/');
+      navigate('/login');
     }
   }, [token]);
 
@@ -51,7 +53,9 @@ function ListaTemas() {
         <div className="container flex flex-col">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {temas.map((tema) => (
+              <>
                 <CardTemas key={tema.id} tema={tema} />
+              </>
             ))}
           </div>
         </div>
